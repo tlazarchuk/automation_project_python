19ca802aa7dd66f4d38fa95d4e212d275bc10490
Taras <taraslaz10@ukr.net>
2021-06-19 21:53:24 +0300

    Upgrade tests

--- go_to_home_page.py
+++ go_to_home_page.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 
+from pages import base_page as bp
 from pages import home_page as hp
 
 class GotoHomePage(unittest.TestCase):
@@ -10,6 +11,7 @@ class GotoHomePage(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.home_page = hp.HomePage(self.driver)
+        self.base_page = bp.BasePage(self.driver)
         self.home_page.go_to_home_page()
         self.driver.implicitly_wait(30)
 
@@ -19,12 +21,12 @@ class GotoHomePage(unittest.TestCase):
 
     def test_go_to_home_page(self):
         current_url = self.driver.current_url
-        self.assertEqual(current_url, "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
+        self.assertEqual(current_url, bp.BASE_URL)
 
     def test_check_home_button(self):
         self.home_page.click_on_home_button()
         current_url = self.driver.current_url
-        self.assertEqual(current_url, "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
+        self.assertEqual(current_url, bp.BASE_URL)
 
 if __name__ == "__main__":
   unittest.main()
