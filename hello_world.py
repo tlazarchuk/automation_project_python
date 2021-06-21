a1ca13388490ab0f20c67ae40174684cafee022b
Taras Lazarchuk <32711990+taraslaz10@users.noreply.github.com>
2021-06-21 10:25:20 +0300

    Test home page (#4)
    * 2 tests
    
    * Upgrade tests
    
    * Upgrade tests
    
    * Test home page
    
    * Changes

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-
-driver = webdriver.Chrome()
-driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
-time.sleep(5)
-
-driver.close()
