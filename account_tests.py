4d55ea84420e32dc8b33189353a6a66e560bbea1
Taras <taraslaz10@ukr.net>
2021-07-04 11:50:00 +0300

    Merge development

--- account_tests.py
+++ account_tests.py
@@ -38,91 +38,20 @@ class CorrectnessUrlManagerTests(unittest.TestCase):
         current_url = self.driver.current_url
         self.assertEqual(current_url, bp.TRANSACTION_URL)
 
-    # def test_make_deposit(self):
-    #     current_money = self.account_page.get_money_balance() 
-    #     self.account_page.click_on_deposit_button()
-    #     self.account_page.fill_currency_for_deposit_or_withdrawl(300)
-    #     self.account_page.click_on_confirm_button()
-    #     current_money += 300
-    #     balance_money = self.account_page.get_money_balance()
-    #     self.assertEqual(current_money, balance_money)
-
-    # def test_make_withdrawl(self):
-    #     current_money = self.account_page.get_money_balance() 
-    #     self.account_page.click_on_withdrawl_button()
-    #     self.account_page.fill_currency_for_deposit_or_withdrawl(300)
-    #     self.account_page.click_on_confirm_button()
-    #     current_money -= 300
-    #     balance_money = self.account_page.get_money_balance()
-    #     self.assertEqual(current_money, balance_money)
-
-    # def test_make_withdrawl_all(self):
-    #     current_money = self.account_page.get_money_balance() 
-    #     self.account_page.click_on_withdrawl_button()
-    #     self.account_page.fill_currency_for_deposit_or_withdrawl(int(current_money))
-    #     self.account_page.click_on_confirm_button()
-    #     current_money = 0
-    #     balance_money = self.account_page.get_money_balance()
-    #     self.assertEqual(current_money, balance_money)
-
-    # def test_make_withdrawl_more_than_balance(self):
-    #     current_money = self.account_page.get_money_balance() 
-    #     self.account_page.click_on_withdrawl_button()
-    #     self.account_page.fill_currency_for_deposit_or_withdrawl(int(current_money)+1)
-    #     self.account_page.click_on_confirm_button()
-    #     balance_money = self.account_page.get_money_balance()
-    #     self.assertEqual(current_money, balance_money)
-
-    # def test_back_button(self):
-    #     self.account_page.click_on_transaction_button()
-    #     self.transaction_page.click_on_back_button()
-    #     self.base_page.wait_for_url(bp.ACCOUNT_URL)
-    #     current_url = self.driver.current_url
-    #     self.assertEqual(current_url, bp.ACCOUNT_URL)
-
-    def test_transaction_history_first(self):
-        self.account_page.click_on_transaction_button()
-        self.transaction_page.click_on_reset_button()
-        self.transaction_page.click_on_back_button()
+    def test_make_deposit(self):
+        current_money = self.account_page.get_money_balance() 
         self.account_page.click_on_deposit_button()
         self.account_page.fill_currency_for_deposit_or_withdrawl(300)
         self.account_page.click_on_confirm_button()
-        self.account_page.click_on_transaction_button()
-        transaction_type = self.transaction_page.get_first_in_list_transaction_type()
-        self.assertEqual(transaction_type, 'Credit')
+        current_money += 300
+        balance_money = self.account_page.get_money_balance()
+        self.assertEqual(current_money, balance_money)
 
-    def test_transaction_history_second(self):
-        self.account_page.click_on_transaction_button()
-        self.transaction_page.click_on_reset_button()
-        self.transaction_page.click_on_back_button()
-        self.account_page.click_on_deposit_button()
-        self.account_page.fill_currency_for_deposit_or_withdrawl(300)
-        self.account_page.click_on_confirm_button()
+    def test_make_withdrawl(self):
+        current_money = self.account_page.get_money_balance() 
         self.account_page.click_on_withdrawl_button()
         self.account_page.fill_currency_for_deposit_or_withdrawl(300)
         self.account_page.click_on_confirm_button()
-<<<<<<< HEAD
-        self.account_page.click_on_transaction_button()
-        transaction_type = self.transaction_page.get_second_in_list_transaction_type()
-        self.assertEqual(transaction_type, 'Debit')
-
-    # def test_reset_button(self):
-    #     self.account_page.click_on_transaction_button()
-    #     self.transaction_page.click_on_reset_button()
-    #     self.transaction_page.click_on_back_button()
-    #     self.account_page.click_on_deposit_button()
-    #     self.account_page.fill_currency_for_deposit_or_withdrawl(300)
-    #     self.account_page.click_on_confirm_button()
-    #     self.account_page.click_on_transaction_button()
-    #     self.transaction_page.click_on_reset_button()
-    #     boolearn = self.transaction_page.find_first_in_list_transaction_type()
-    #     self.assertFalse(boolearn)
-
-    # def test_change_account_number(self):
-    #     self.account_page.click_on_account_number_1002()
-    #     account_number = self.account_page.get_account_number()
-    #     self.assertEqual('1002', account_number)
-=======
         current_money -= 300
         balance_money = self.account_page.get_money_balance()
         self.assertEqual(current_money, balance_money)
@@ -174,7 +103,7 @@ class CorrectnessUrlManagerTests(unittest.TestCase):
         self.account_page.click_on_confirm_button()
         self.account_page.click_on_transaction_button()
         transaction_type = self.transaction_page.get_second_in_list_transaction_type()
-        self.assertEqual(transaction_type, 'Dedit')
+        self.assertEqual(transaction_type, 'Debit')
 
     def test_reset_button(self):
         self.account_page.click_on_transaction_button()
@@ -192,7 +121,6 @@ class CorrectnessUrlManagerTests(unittest.TestCase):
         self.account_page.click_on_account_number_1002()
         account_number = self.account_page.get_account_number()
         self.assertEqual('1002', account_number)
->>>>>>> development
 
 if __name__ == "__main__":
   unittest.main()
